b3fd3c31463018207a659e96e810a591f9435bbf
Mawen Salignat-Moandal <mwnslgt@gmail.com>
2024-05-10 09:51:07 +0200

    Express & DB

--- app.js
+++ app.js
@@ -1,32 +1,11 @@
+
 const express = require('express')
-const app = express()
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-const userRouter = require("./Routers/user")
-const taskRouter = require("./Routers/task")
-const mongooseRequiring = require("./db/moongose")
-const tasks = require('./src/Routers/tasks')
-
-const port = process.env.port
-
-
-app.listen(port, console.log(`This is the server for Task manager  ${port}`))
-
-
-app.use(express.json())
-app.use(userRouter);
-app.use(taskRouter)
-
-
-
-
-// routes 
-
-app.get('/hello', (req,res)=>  {
-    res.send('Task manager first routes')
+app.listen(PORT, () => {
+    console.log(`Le serveur est en train de se lancer dans le port ${PORT}`)
 })
-
-app.use('/api/v1/tasks', tasks)
-
 
 
 
--- src/db/mongoose.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.connect.env.MONGODB_URL, {
-    useNewUrlParser : true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-}
